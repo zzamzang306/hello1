53e0387320712d40c0c2024bc8cc4e4c2090160a
Changsu Kim <zzamzang306@hotmail.com>
2020-08-19 23:58:37 +0000

    changed made

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author : Changsu Kim
+ *
+ * 2020 / 08 / 18
+ *
+ * This is testing for TA job
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv){
+     int num;
+     printf("What is your favorite number: ");
+     scanf("%d\n", &num);
+     printf("Thanks");
+
+     return 0;
+ }
