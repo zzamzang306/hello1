1e5e6ff83539429460c1cff645143244d2765c84
Changsu Kim <zzamzang306@hotmail.com>
2020-08-20 15:25:23 +0000

    testing

--- hello.c
+++ hello.c
@@ -9,11 +9,17 @@
  #include <stdlib.h>
  #include <stdio.h>
 
+#define KMS_PER_MILE 1.60934
+
  int main(int argc, char **argv){
-     int num;
-     printf("What is your favorite number: ");
-     scanf("%d\n", &num);
-     printf("Thanks");
+     double miles, kms;
+
+     printf("Please enter miles: ");
+     scanf("%lf", &miles);
+
+     kms = KMS_PER_MILE * miles;
+
+     printf("%f miles is equal to %f kilometers\n", miles, kms);
 
      return 0;
  }
